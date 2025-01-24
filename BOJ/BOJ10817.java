2a7a97c2c8b22ffbfc29e8bdc134c433a77b743b
minhe8564 <minhe8564@gmail.com>
2025-01-25 00:20:37 +0900

    세 수

---
+++ BOJ/BOJ10817.java
@@ -0,0 +1,20 @@
+package algorithm;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ10817 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int[] arr = new int[3];
+		for(int i = 0; i < 3; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		System.out.println(arr[1]);
+		br.close();
+	}
+
+}
