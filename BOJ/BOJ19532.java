2634001a07f8d9d5cd845945c02df50cc7c70c40
minhe8564 <minhe8564@gmail.com>
2025-02-03 13:42:58 +0900

    수학은 비대면강의입니다

---
+++ BOJ/BOJ19532.java
@@ -0,0 +1,34 @@
+package algorithm;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ19532 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		int f = Integer.parseInt(st.nextToken());
+				
+		for(int x = -999; x <= 999; x++) {
+			for(int y = -999; y <= 999; y++) {
+				if(a*x + b*y == c) {
+					if(d*x + e*y == f) {
+						sb.append(x).append(" ").append(y);
+					}
+				}
+			}
+		}
+		
+		System.out.println(sb);
+		br.close();
+	}
+
+}
