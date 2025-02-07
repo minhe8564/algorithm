c5db8e008c9cf5e373c25421e12052867e65bb60
minhe8564 <minhe8564@gmail.com>
2025-02-07 23:35:26 +0900

    대표값2

---
+++ BOJ/BOJ2587.java
@@ -0,0 +1,25 @@
+package algorithm;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ2587 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] num = new int[5];
+		int sum = 0;
+		for(int i = 0; i < 5; i++) {
+			num[i] = Integer.parseInt(br.readLine());
+			sum += num[i];
+		}
+		
+		Arrays.sort(num);
+		
+		System.out.println(sum / 5);
+		System.out.println(num[2]);
+		
+		br.close();
+	}
+
+}
