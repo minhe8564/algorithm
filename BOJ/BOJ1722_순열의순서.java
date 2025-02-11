fa2eb412f894453aa37c4af27712b7c369a6dfb6
minhe8564 <minhe8564@gmail.com>
2025-02-12 01:26:56 +0900

    순열의 순서

--- BOJ/BOJ1722_순열의순서.java
+++ BOJ/BOJ1722_순열의순서.java
@@ -24,16 +24,19 @@ public class BOJ1722_순열의순서 {
         if (command == 1) {
             long k = Long.parseLong(st.nextToken()); // k번째 순열 찾기
             
+            // 순열 채우기
             for (int i = 0; i < N; i++) {
+            	// 1~N까지 중에서 아직 사용되지 않은 숫자 선택
                 for (int j = 1; j <= N; j++) {
-                    if (visited[j]) continue;
+                    if (visited[j]) continue; // 이미 사용된 숫자 건너뜀
                     
+                    // 현재 위치에서 (N-i-K)!만큼의 경우의 수 확인
                     if (fac[N - i - 1] < k) {
-                        k -= fac[N - i - 1];
+                        k -= fac[N - i - 1]; // k번째가 뒤족에 있으므로 점프
                     } else {
-                        per[i] = j;
-                        visited[j] = true;
-                        break;
+                        per[i] = j; // 현재 위치에 숫자 j 선택
+                        visited[j] = true; // 숫자 j 사용완료
+                        break; // 다음 자리 숫자 선택
                     }
                 }
             }
@@ -51,10 +54,10 @@ public class BOJ1722_순열의순서 {
             long answer = 1;
             for (int i = 0; i < N; i++) {
                 for (int j = 1; j < per[i]; j++) {
-                    if (visited[j]) continue;
-                    answer += fac[N - i - 1];
+                    if (visited[j]) continue; // 이미 사용된 숫자 건너뜀
+                    answer += fac[N - i - 1]; // 해당 숫자가 앞에 올 수 있는 경우의 수 더하기
                 }
-                visited[per[i]] = true;
+                visited[per[i]] = true; // 현재 숫자 사용완료
             }
             
             System.out.println(answer);
