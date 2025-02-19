d3b0fb43f4bc222cce9c0bdc956bf02f0f35b015
minhe8564 <minhe8564@gmail.com>
2025-02-19 22:24:31 +0900

    N과 M (4)

---
+++ BOJ/BOJ15651_N과M4.java
@@ -0,0 +1,44 @@
+package algorithm;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ15651_N과M4 {
+	static int N, M;
+	static int[] arr;
+	static int[] result;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = i + 1;
+		}
+		result = new int[M];
+
+		perm(0, 0);
+
+		System.out.println(sb);
+		br.close();
+	}
+
+	private static void perm(int cnt, int start) {
+		if (cnt == M) {
+			for (int i : result) {
+				sb.append(i).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = start; i < N; i++) {
+			result[cnt] = arr[i];
+			perm(cnt + 1, i);
+		}
+	}
+
+}
