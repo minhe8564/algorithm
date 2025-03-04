80c816303ab02d6b8ac29b957cc5323fbf9b7d8e
minhe8564 <minhe8564@gmail.com>
2025-03-04 17:08:21 +0900

    선수과목

--- BOJ/BOJ14567_선수과목.java
+++ BOJ/BOJ14567_선수과목.java
@@ -1,33 +1,36 @@
+/*
+ * 위상정렬
+ */
+
 import java.io.*;
 import java.util.*;
 
 public class BOJ14567_선수과목 {
 	static int N, M;
-	static int[] indegree;
 	static List<List<Integer>> graph;
-	
+	static int[] indegree;
+	static int[] result;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		indegree = new int[N+1];
 		graph = new ArrayList<List<Integer>>();
 		for(int i = 0; i <= N; i++) {
 			graph.add(new ArrayList<Integer>());
 		}
-		
+		indegree = new int[N+1];
+		result = new int[N+1];
 		for(int m = 0; m < M; m++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			
 			graph.get(a).add(b);
 			indegree[b]++;
 		}
 		
-		int[] result = new int[N+1];
 		Queue<Integer> q = new ArrayDeque<Integer>();
 		for(int i = 1; i <= N; i++) {
 			if(indegree[i] == 0) {
@@ -42,8 +45,8 @@ public class BOJ14567_선수과목 {
 			for(int i = 0; i < graph.get(curr).size(); i++) {
 				int next = graph.get(curr).get(i);
 				indegree[next]--;
-				result[next] = Math.max(result[next], result[curr]+1);
-				if(indegree[next] == 0) {
+				result[next] = Math.max(result[next], result[curr]+1); // 가장 늦게 끝나는 선수과목 기준으로 학기를 계산
+				if(indegree[next]==0) {
 					q.offer(next);
 				}
 			}
