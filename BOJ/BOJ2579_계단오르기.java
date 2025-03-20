53e11cf64390bdd007e6516500dc4d95f08d5c89
minhe8564 <minhe8564@gmail.com>
2025-03-20 21:40:38 +0900

    계단 오르기

--- BOJ/BOJ2579_계단오르기.java
+++ BOJ/BOJ2579_계단오르기.java
@@ -1,8 +1,19 @@
 /*
+ * BOJ 2579번 : 계단 오르기 
+ * 메모리 : 11,612kb
+ * 시간 : 64ms
+ * 
  * 1. 계단을 오를 때 1칸, 2칸 오르기 가능
  * 2. 1칸 3번 연속으로 불가능
- * 3. 마지막 계단은 반드시 밝아야 한다.
+ * 3. 마지막 계단은 반드시 아야 한다.
+ * 
+ * n-1에서 n 올라가는 경우 (n-2는 건너뛰어야 함)
+ * dp[n-3]+stair[n-1]+start[n]
+ * 
+ * n-2에서 n 올라가는 경우
+ * dp[n-2]+stair[n]
  */
+
 package algorithm;
 
 import java.io.*;
@@ -12,7 +23,8 @@ public class BOJ2579_계단오르기 {
 	static int N;
 	static int[] stair;
 	static int[] dp;
-	
+	static int answer;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -22,25 +34,17 @@ public class BOJ2579_계단오르기 {
 			stair[n] = Integer.parseInt(br.readLine());
 		}
 		
-		if(N==1) {
-			System.out.print(stair[1]);
-			return;
-		}
-		
 		dp = new int[N+1];
 		dp[1] = stair[1];
 		dp[2] = stair[1]+stair[2];
 		
-		// 마지막 계단을 밟기 위한 경우 
-		// 1. i-1에서 올라오는 경우 : i-2는 건너뛰어야 한다. 
-		// 2. i-2에서 올라오는 경우
-		for(int i = 3; i <= N; i++) {
-			dp[i] = Math.max(dp[i-3] + stair[i-1] + stair[i], dp[i-2] + stair[i]);
+		for(int n = 3; n <= N; n++) {
+			dp[n] = Math.max(dp[n-3]+stair[n-1]+stair[n], dp[n-2]+stair[n]);
 		}
 		
-		sb.append(dp[N]);
+		answer = dp[N];
+		sb.append(answer);
 		System.out.print(sb);
 		br.close();
 	}
-
 }
