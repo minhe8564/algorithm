4830391766edf3368f1ffc8be35ed084cee869c7
minhe8564 <minhe8564@gmail.com>
2025-03-20 21:42:15 +0900

    가장 긴 증가하는 부분 수열_이분탐색

--- BOJ/BOJ11053_가장긴증가하는부분수열_이분탐색.java
+++ BOJ/BOJ11053_가장긴증가하는부분수열_이분탐색.java
@@ -1,4 +1,5 @@
 /*
+ * BOJ 11053번 : 가장 긴 증가하는 부분 수열 
  * 이분탐색 O(NlogN)
  * 메모리 : 11,832kb
  * 시간 : 72ms
@@ -18,7 +19,7 @@ public class BOJ11053_가장긴증가하는부분수열_이분탐색 {
 		StringBuilder sb = new StringBuilder();
 		N = Integer.parseInt(br.readLine());
 		A = new int[N];
-		LIS = new int[N];
+		LIS = new int[N]; // 현재까지의 증가하는 부분 수열 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for(int n = 0; n < N; n++) {
 			A[n] = Integer.parseInt(st.nextToken());
@@ -26,10 +27,14 @@ public class BOJ11053_가장긴증가하는부분수열_이분탐색 {
 		
 		LIS[0] = A[0];
 		int length = 1;
+		
 		for(int n = 1; n < N; n++) {
+			// 현재 값이 LIS 배열 마지막 값 보다 크면 -> LIS 추가 
 			if(A[n] > LIS[length-1]) {
 				LIS[length++] = A[n];
 			} else {
+				// 현재 값이 LIS 중간에 들어가야할 경우 -> 이분탐색으로 위치 찾기 
+				// 가능한 작은 값으로 수열 유지해야 뒤에 더 많은 수가 올 수 있다
 				int pos = binarySearch(0, length-1, A[n]);
 				LIS[pos] = A[n];
 			}
@@ -44,9 +49,9 @@ public class BOJ11053_가장긴증가하는부분수열_이분탐색 {
 	private static int binarySearch(int left, int right, int target) {
 		while(left < right) {
 			int mid = (left+right)/2;
-			if(LIS[mid] < target) {
+			if(LIS[mid] < target) { // 오른쪽 이동 
 				left = mid+1;
-			} else {
+			} else { // 왼쪽 이동 
 				right = mid;
 			}
 		}
