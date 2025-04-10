2f13e73c635e12c28de4bb3c16c37efcedc216ee
minhe8564 <minhe8564@gmail.com>
2025-04-10 09:45:42 +0900

    대표값 2

--- BOJ/BOJ2587_대표값2.java
+++ BOJ/BOJ2587_대표값2.java
@@ -3,7 +3,7 @@ package algorithm;
 import java.io.*;
 import java.util.*;
 
-public class BOJ2587 {
+public class BOJ2587_대표값2 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
