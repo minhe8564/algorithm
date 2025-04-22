89a3b89497e97914855e106516fa9c7f24b65ed4
minhe8564 <minhe8564@gmail.com>
2025-04-22 13:52:05 +0900

    사과나무

---
+++ BOJ/BOJ19539_사과나무.java
@@ -0,0 +1,35 @@
+/*
+ * BOJ 19539번 : 사과나무
+ * 메모리 : 22,760kb
+ * 시간 : 192ms
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ19539_사과나무 {
+	static int N;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(br.readLine());
+		int one = 0, two = 0, sum = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int n = 0; n < N; n++) {
+			int num = Integer.parseInt(st.nextToken());
+			sum += num;
+			one += num%2;
+			two += num/2;
+		}
+		
+		if(sum%3 > 0 || one > two) {
+			sb.append("NO");
+		} else {
+			sb.append("YES");
+		}
+		
+		System.out.print(sb);
+		br.close();
+	}
+}
